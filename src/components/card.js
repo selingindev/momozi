025ad1746af5bd0e90091cc8ee5ebe860db428e4
selingindev <88356338+selingindev@users.noreply.github.com>
2025-09-25 22:59:16 -0300

    container para state

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,21 @@
 import "./card.css"
-
-export default function Card({title, image}){
-    return(
-        <div className="CardStyle" style={{backgroundImage: `url(${image})`}} >
-            <div className="DownSection">
-                <h2 className> {title} </h2>
-            </div>
+import { useState } from 'react'
+export default function Card({ title, image }) {
+    var [isOpen, setIsOpen] = useState(false);
+    return (
+        <div className="ContainerCondition">
+            {isOpen = false ?
+            <h1>ola</h1>
+            :
+            <button className="ButtonImage" onClick={ () => setIsOpen(isOpen === true) }  >
+                
+                <img className="CardStyle" src={image} />
+                
+                <div className="DownSection">      
+                    <h2 className> {title} </h2>
+                </div>
+            </button>
+            }
         </div>
     )
 }
